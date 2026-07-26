c8ba740abc243dee3e850d47ea852d75d26df440
Andrea Epiayu <155672941+Andrepiaaa@users.noreply.github.com>
2026-07-26 15:45:55 +0000

    feat: renderizadi funcional de la cara del robotChip

--- main.go
+++ main.go
@@ -7,8 +7,11 @@ func main() {
 }
 
 func chip() {
-	fmt.Println("        ▛▀▀▀▀▀▀▀▀▀▀▀▜")
-	fmt.Println("        ▌  ◉     ◉  ▐")
-	fmt.Println("        ▌     ◡     ▐")
-	fmt.Println("        ▙▄▄▄▄▄▄▄▄▄▄▄▟")
+	    rebot := `
+	 ▛▀▀▀▀▀▀▀▀▀▀▀▜")
+	 ▌  ◉     ◉  ▐")
+	 ▌     ◡     ▐")
+	 ▙▄▄▄▄▄▄▄▄▄▄▄▟")
+`
+fmt.Println(rebot)
 }
